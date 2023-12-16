955a29e8bddeeb4eddd99c3ed74ffc96a4963683
aShinde <ananta.shinde@honeywellcom>
2023-12-16 10:00:51 +0530

    16/12/23

---
+++ demos/2Darray.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    int numbers[5];
+    int marks[3][3];
+    numbers[1] = 20;
+    printf("enter marks details");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+             printf("\nenter practicle marks for subject %d",i+1);
+             scanf("%d",&marks[i][j]);
+        }   
+    }
+    
+     
+    for(int i=0;i<3;i++)
+    {
+        int sum = 0;
+        for(int j=0;j<3;j++)
+        {
+              sum = sum + marks[i][j];
+        }
+        printf("subject %d :%d\n",i+1,sum);
+    }
+
+
+    return 0;
+}
